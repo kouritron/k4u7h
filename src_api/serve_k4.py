e8592b8e511d5d8607ef691711077cec6b75f282
kouritron <kouritron@users.noreply.github.com>
2024-05-17 23:55:21 -0400

    serv k4 tornado app

---
+++ src_api/serve_k4.py
@@ -0,0 +1,31 @@
+import os
+import time
+import random
+import logging
+
+import tornado.httpserver as tserver
+import tornado.web as tweb
+from tornado.ioloop import IOLoop
+
+from libk4u7h.logutilz import ANSI_COLORS
+from libk4u7h import paramz
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+def main():
+    print(f"0====>>>>> serve_k4.py: starting 1 proc server ...\n")
+
+    app = tweb.Application([], debug=False)
+
+    http_server = tserver.HTTPServer(app)
+    http_server.bind(paramz.K4_DEFAULT_PORT)
+    http_server.start(1)
+    IOLoop.current().start()
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+
+if '__main__' == __name__:
+    main()
