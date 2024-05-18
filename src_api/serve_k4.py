9c8ceaf2220136f4ea62f0be085ed4e243a1f51b
kouritron <kouritron@users.noreply.github.com>
2024-05-18 08:35:34 -0400

    tornado server start

--- src_api/serve_k4.py
+++ src_api/serve_k4.py
@@ -3,12 +3,14 @@ import time
 import random
 import logging
 
-import tornado.httpserver as tserver
 import tornado.web as tweb
+import tornado.httpserver as tserver
 from tornado.ioloop import IOLoop
 
-from libk4u7h import paramz
 from libk4u7h.logutilz import simple_log as log
+from libk4u7h import paramz
+from libk4u7h import k4routes
+
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -16,11 +18,17 @@ from libk4u7h.logutilz import simple_log as log
 def main():
     log.info(f"*** serve_k4.py: starting 1 proc server ...\n")
 
-    app = tweb.Application([], debug=False)
+    exposed_routes = [
+        (r"/", k4routes.core.IndexHandler),
+    ]
 
+    app = tweb.Application(exposed_routes, debug=paramz.TORNADO_DEBUG_MODE)
+
+    # starting server this way works well w. both single and multi process. just pass the number of pids. 
     http_server = tserver.HTTPServer(app)
     http_server.bind(paramz.K4_DEFAULT_PORT)
     http_server.start(1)
+
     IOLoop.current().start()
 
 
