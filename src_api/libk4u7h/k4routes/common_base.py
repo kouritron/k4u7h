a68eab789875955e97abaaeed204ae439f542f81
kouritron <kouritron@users.noreply.github.com>
2024-05-19 03:20:25 -0400

    implement options on all routes

--- src_api/libk4u7h/k4routes/common_base.py
+++ src_api/libk4u7h/k4routes/common_base.py
@@ -1,7 +1,13 @@
 import tornado.web as tweb
 
+from ..logutilz import simple_log as log
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 class K4BaseHandler(tweb.RequestHandler):
-    pass
+
+    def options(self, *args, **kwargs):
+        """ Allow and implement HTTP OPTIONS request on all resources on this server. """
+
+        self.respond()
