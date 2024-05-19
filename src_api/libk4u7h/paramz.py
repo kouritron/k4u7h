a580272d0ae4ae9cf527a6b1ddec4e87e9002be2
kouritron <kouritron@users.noreply.github.com>
2024-05-19 01:20:27 -0400

    paramz for static

--- src_api/libk4u7h/paramz.py
+++ src_api/libk4u7h/paramz.py
@@ -1,5 +1,9 @@
 
+import os
+import subprocess as sp
+from pathlib import Path
 
+from .logutilz import simple_log as log
 
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
@@ -9,13 +13,18 @@
 # - 15908  API
 
 K4_DEFAULT_PORT = 159_80
-
 TORNADO_DEBUG_MODE = True
 
+REPO_ROOT_PATH = Path(sp.check_output("git rev-parse --show-toplevel", shell=True).decode()).resolve()
+STATIC_DIR_PATH = (REPO_ROOT_PATH / 'build_tmp' / 'static').resolve()
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
-
+log.info("k4 paramz subsystem initialized.")
+log.info(f"repo root is at: {REPO_ROOT_PATH}")
+log.info(f"static directory is set to: {STATIC_DIR_PATH}")
 
 
 
