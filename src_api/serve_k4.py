7c0a8eb78e199d8d4e50061b261b9f59b0c0d102
kouritron <kouritron@users.noreply.github.com>
2024-05-20 20:47:28 -0400

    serv k4

--- src_api/serve_k4.py
+++ src_api/serve_k4.py
@@ -5,7 +5,7 @@ import logging
 
 import tornado.web as tweb
 import tornado.httpserver as tserver
-from tornado.ioloop import IOLoop
+from tornado.ioloop import IOLoop as TorIOLoop
 
 from libk4u7.logutilz import simple_log as log
 from libk4u7 import paramz
@@ -16,29 +16,35 @@ from libk4u7 import k4routes
 # ----------------------------------------------------------------------------------------------------------------------
 def init_3rd_party_loggers():
     """ Enable/disable loggers from tornado and/or other packages outside libk4u7 """
-    
-    loggers_list = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
-    for x in loggers_list:
-        print(x)
 
-    
-    # print(logging.getLogger("tornado.application").disabled)
+    logging.getLogger("tornado.application").setLevel(logging.INFO)
+    logging.getLogger("tornado.general").setLevel(logging.INFO)
+    logging.getLogger("tornado.access").setLevel(logging.INFO)
+    logging.getLogger("tornado").setLevel(logging.INFO)
+
+    # loggers_list = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+    # for x in loggers_list:
+    #     print(x)
+
+    # by default most are enabled, and set to WARNING level. Some of the loggers:
+    # concurrent concurrent.futures asyncio tornado.access tornado tornado.application tornado.general
+
+    # To disable
     # logging.getLogger("tornado.application").disabled = True
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 def main():
-    log.info(f"*** serve_k4.py: starting 1 proc server ...\n")
 
     init_3rd_party_loggers()
 
+    # ----- setup tornado app
     tor_settings = {
         # Enable debug mode for better error messages, disable useless autoreload.
         "debug": True,
         "autoreload": False,
     }
-
 
     exposed_routes = [
         (r"/", k4routes.core.IndexHandler),
@@ -48,14 +54,18 @@ def main():
         }),
     ]
 
-    app = tweb.Application(exposed_routes, **tor_settings)
+    k4app = tweb.Application(exposed_routes, **tor_settings)
 
-    # This works fine w/ both single and multi process servers. Just pass the number of pids to start()
-    http_server = tserver.HTTPServer(app)
+    # ----- start listening
+    log.info(f"*** serve_k4.py: starting 1 proc server ...\n")
+
+    # This works fine w/ both single and multi process servers.
+    # Just pass the number of pids to start()
+    http_server = tserver.HTTPServer(k4app)
     http_server.bind(paramz.K4_DEFAULT_PORT)
     http_server.start(1)
 
-    IOLoop.current().start()
+    TorIOLoop.current().start()
 
 
 # ----------------------------------------------------------------------------------------------------------------------
